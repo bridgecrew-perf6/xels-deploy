79dd2ce959a01e69a216d5542eff1cf58686de43
Nahidul Islam Prince <38788958+nahid9300@users.noreply.github.com>
2022-04-11 11:15:11 +0600

    Target Framework version bug fixed and VS Dataflowblock bug fixed.

---
+++ src/Xels.SmartContracts.CLR.Tests/Loader/Test.cs
@@ -0,0 +1,11 @@
+﻿using Stratis.SmartContracts;
+
+namespace Xels.SmartContracts.CLR.Tests.Loader
+{
+    public class Test : SmartContract
+    {
+        public Test(ISmartContractState state)
+            : base(state)
+        { }
+    }
+}
